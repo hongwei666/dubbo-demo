ee40a36a40f57d7314148cb9764efbe3ea6d876e
hongwei.gao <ghwmail@163.com>
2018-08-01 18:50:22 +0800

    dubbo提供的新服务

--- dubbo-provider/src/main/java/com/provider/service/impl/UserServiceImpl.java
+++ dubbo-provider/src/main/java/com/provider/service/impl/UserServiceImpl.java
@@ -4,6 +4,6 @@ import com.api.service.UserService;
 
 public class UserServiceImpl implements UserService {
     public String getName(int userId) {
-        return "dubbo提供的服务"+userId;
+        return "dubbo提供的new服务"+userId;
     }
 }
